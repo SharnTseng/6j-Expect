8b7407ec63c2f16b4310de9f1bbd06d645b32d7f
Sharn Tseng <54creepersam@gmail.com>
2024-08-31 12:45:18 +0800

    6階熔煉期望值.cpp

--- 6階熔煉期望值.cpp
+++ 6階熔煉期望值.cpp
@@ -3,38 +3,42 @@
 #include <stdlib.h>
 int main() {
 	srand(time(NULL));
-	int a,g,d,tg,td,r=0;
-	printf("Number of 6j? : ");
+	int a,g,d,tg,td,r=0,t,tt;
+	printf("幾顆6階?(熔煉直到成功) : ");
     scanf("%d",&a);
     for(int i=0;i<a;i++){
-    	g=3;
+    	g=3; //初始條件 三顆5j 一張卷 嘗試0次
     	d=1;
+	t=0;
         while(1){
-            r=rand() % 100 + 1;
-            if (r<=20){
-            	break;
-			}
-			else if(r<=40){
-				d+=1;
-			}
-			else if(r<=70){
-				g+=1;
-				d+=1;
-			}
-			else if(r<=90){
-				g+=2;
-				d+=1;
-			}
-			else if(r<=100){
-				g+=3;
-				d+=1;
-			}
+        	r=rand() % 100 + 1;//1~100 取隨機
+		t++;
+        	if (r<=20){ //20%成功
+            		break;
+		}
+		else if(r<=40){ //20%原封不動 耗一張卷
+			d+=1;
+		}
+		else if(r<=70){ //30%耗一顆一張
+			g+=1;
+			d+=1;
+		}
+		else if(r<=90){ //20%耗兩顆一張
+			g+=2;
+			d+=1;
+		}
+		else if(r<=100){ //10%耗三顆一張
+			g+=3;
+			d+=1;
+		}
         }
-        tg+=g;
+        tg+=g; //將結果加到總和
         td+=d;
+	tt+=t;
     }
-	printf("Total 5j cost: %d  Total Dwarf cost: %d\n",tg,td);
-	printf("Average 5j: %.2f  Average Dwarf: %.2f\n",float(tg)/a,float(td)/a);
-	printf("Average $ cost: %.2f w\n",(float(tg)/a+float(td)/a)*100 );
+	printf("共嘗試融煉了: %d 次\n",tt);
+	printf("總消耗5階量: %d 顆   總消耗卷: %d 張\n",tg,td);
+	printf("平均消耗5階量: %.2f 顆  平均消耗卷量: %.2f 張\n",float(tg)/a,float(td)/a);
+	printf("平均熔煉一顆要消耗 %.2f 萬\n",(float(tg)/a+float(td)/a)*100 );
     return 0;
 }
